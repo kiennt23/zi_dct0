ed9934cc1fb7b4ca4950489a5246f46675b5b5af
Kien Nguyen <kiennt23@gmail.com>
2018-06-03 16:13:44 +0800

    Use array instead of tuple for setuptools classifier

--- setup.py
+++ setup.py
@@ -9,13 +9,12 @@ setuptools.setup(
     author="Kien Nguyen",
     author_email="kiennt23@gmail.com",
     description="ZI DCT0 implementation in python",
-    long_description="ZI DCT0 implementation in python",
-    long_description_content_type="text/markdown",
+    long_description=long_description,
     url="https://github.com/kiennt23/zi_dct0.git",
     packages=setuptools.find_packages(),
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
-    ),
+    ],
 )
