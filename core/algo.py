70f571fd0cf12d05c3ca89b8f8dc1d5ef2e56ae9
Kien Nguyen <kiennt23@gmail.com>
2018-06-17 14:23:34 +0700

    1. Object oriented core.algo module 2. Add unit testing for core.algo module

--- core/algo.py
+++ core/algo.py
@@ -8,77 +8,74 @@ class DCEventType(Enum):
     OVERSHOOT = 3
 
 
-class TradeMethod(Enum):
+class TradeStrategy(Enum):
     TF = 1
     CT = 2
 
 
 class Config:
-    def __init__(self, trade_method, delta_p, mode=None, p_ext=None):
-        self.trade_method = trade_method
+    def __init__(self, strategy, delta_p, initial_mode=None, initial_p_ext=None):
+        self.strategy = strategy
         self.delta_p = delta_p
-        if mode is None and p_ext is None:
-            if trade_method == TradeMethod.TF:
-                self.mode = DCEventType.UPTURN
-                self.p_ext = 1000000000.0
+        if initial_mode is None and initial_p_ext is None:
+            if strategy == TradeStrategy.TF:
+                self.initial_mode = DCEventType.UPTURN
+                self.initial_p_ext = 0.0
             else:
-                self.mode = DCEventType.DOWNTURN
-                self.p_ext = 0.0
+                self.initial_mode = DCEventType.DOWNTURN
+                self.initial_p_ext = 1000000000.0
         else:
-            self.mode = mode
-            self.p_ext = p_ext
+            self.initial_mode = initial_mode
+            self.initial_p_ext = initial_p_ext
 
 
-config = None
-logger = None
+class ZI_DCT0:
+    def __init__(self, logger, config: Config):
+        self.logger = logger
+        self.config = config
+        self.mode = config.initial_mode
+        self.p_ext = config.initial_p_ext
+        pass
+
+    def observe(self, p_t):
+        if self.config is None:
+            self.logger.error('No configuration')
+            return
+        if self.mode == DCEventType.UPTURN:
+            if p_t <= self.p_ext * (1.0 - self.config.delta_p):
+                self.logger.debug('initial_p_ext={} p_t={}'.format(self.p_ext, p_t))
+                self.mode = DCEventType.DOWNTURN
+                self.p_ext = p_t
+                return self.mode
+            else:
+                self.p_ext = max([self.p_ext, p_t])
+                self.logger.debug('initial_p_ext={} p_t={}'.format(self.p_ext, p_t))
+                return DCEventType.OVERSHOOT
+
+        else:  # initial_mode is DOWNTURN
+            if p_t >= self.p_ext * (1.0 + self.config.delta_p):
+                self.logger.debug('initial_p_ext={} p_t={}'.format(self.p_ext, p_t))
+                self.mode = DCEventType.UPTURN
+                self.p_ext = p_t
+                return self.mode
+            else:
+                self.p_ext = min([self.p_ext, p_t])
+                self.logger.debug('initial_p_ext={} p_t={}'.format(self.p_ext, p_t))
+                return DCEventType.OVERSHOOT
+        pass
+
+    def is_buy_signaled(self):
+        buy_signaled = (TradeStrategy.TF == self.config.strategy and DCEventType.UPTURN == self.mode) or (
+            TradeStrategy.CT == self.config.strategy and DCEventType.DOWNTURN == self.mode)
+        return buy_signaled
+
+    def is_sell_signaled(self):
+        sell_signaled = (TradeStrategy.TF == self.config.strategy and DCEventType.DOWNTURN == self.mode) or (
+                TradeStrategy.CT == self.config.strategy and DCEventType.UPTURN == self.mode)
+        return sell_signaled
 
 
 class Position:
     def __init__(self, price):
         self.price = price
 
-
-def config_log(log_level):
-    global logger
-    logging.basicConfig(level=log_level)
-    logger = logging.getLogger(__name__)
-
-
-def zi_dct0(p_t):
-    global config, logger
-    if config is None:
-        logger.error('No configuration')
-        return
-    if config.mode == DCEventType.UPTURN:
-        if p_t <= config.p_ext * (1.0 - config.delta_p):
-            logger.debug('p_ext={} p_t={}'.format(config.p_ext, p_t))
-            config.mode = DCEventType.DOWNTURN
-            config.p_ext = p_t
-            return config.mode
-        else:
-            config.p_ext = max([config.p_ext, p_t])
-            logger.debug('p_ext={} p_t={}'.format(config.p_ext, p_t))
-            return DCEventType.OVERSHOOT
-
-    else:  # mode is DOWNTURN
-        if p_t >= config.p_ext * (1.0 + config.delta_p):
-            logger.debug('p_ext={} p_t={}'.format(config.p_ext, p_t))
-            config.mode = DCEventType.UPTURN
-            config.p_ext = p_t
-            return config.mode
-        else:
-            config.p_ext = min([config.p_ext, p_t])
-            logger.debug('p_ext={} p_t={}'.format(config.p_ext, p_t))
-            return DCEventType.OVERSHOOT
-
-
-def is_buy_signaled(event_type, trade_method):
-    buy_signaled = (trade_method == TradeMethod.TF and DCEventType.UPTURN == event_type) or (
-            trade_method == TradeMethod.CT and DCEventType.DOWNTURN == event_type)
-    return buy_signaled
-
-
-def is_sell_signaled(event_type, trade_method):
-    sell_signaled = (trade_method == TradeMethod.TF and DCEventType.DOWNTURN == event_type) or (
-            trade_method == TradeMethod.CT and DCEventType.UPTURN == event_type)
-    return sell_signaled
---
+++ test/algo_test.py
@@ -0,0 +1,86 @@
+import logging
+import unittest
+from core.algo import *
+
+
+class ZI_DCT0Test(unittest.TestCase):
+    def setUp(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_should_return_overshoot(self):
+        config = Config(TradeStrategy.TF, 0.01)
+        dct0_runner = ZI_DCT0(self.logger, config)
+        observe = dct0_runner.observe(100)
+        self.assertEqual(DCEventType.UPTURN, dct0_runner.mode)
+        self.assertEqual(100, dct0_runner.p_ext)
+        self.assertEqual(DCEventType.OVERSHOOT, observe)
+        pass
+
+    def test_should_turn_downturn(self):
+        config = Config(TradeStrategy.TF, 0.01)
+        dct0_runner = ZI_DCT0(self.logger, config)
+        price_series = [100, 99.9, 100.1, 99.5, 100.5, 99]
+        observe = dct0_runner.observe(price_series[0])
+        self.assertEqual(DCEventType.UPTURN, dct0_runner.mode)
+        self.assertEqual(100, dct0_runner.p_ext)
+        self.assertEqual(DCEventType.OVERSHOOT, observe)
+        observe = dct0_runner.observe(price_series[1])
+        self.assertEqual(DCEventType.UPTURN, dct0_runner.mode)
+        self.assertEqual(100, dct0_runner.p_ext)
+        self.assertEqual(DCEventType.OVERSHOOT, observe)
+        observe = dct0_runner.observe(price_series[2])
+        self.assertEqual(DCEventType.UPTURN, dct0_runner.mode)
+        self.assertEqual(100.1, dct0_runner.p_ext)
+        self.assertEqual(DCEventType.OVERSHOOT, observe)
+        observe = dct0_runner.observe(price_series[3])
+        self.assertEqual(DCEventType.UPTURN, dct0_runner.mode)
+        self.assertEqual(100.1, dct0_runner.p_ext)
+        self.assertEqual(DCEventType.OVERSHOOT, observe)
+        observe = dct0_runner.observe(price_series[4])
+        self.assertEqual(DCEventType.UPTURN, dct0_runner.mode)
+        self.assertEqual(100.5, dct0_runner.p_ext)
+        self.assertEqual(DCEventType.OVERSHOOT, observe)
+        observe = dct0_runner.observe(price_series[5])
+        self.assertEqual(DCEventType.DOWNTURN, dct0_runner.mode)
+        self.assertEqual(99, dct0_runner.p_ext)
+        self.assertEqual(DCEventType.DOWNTURN, observe)
+        pass
+
+    def test_should_turn_upturn(self):
+        config = Config(TradeStrategy.TF, 0.01, DCEventType.DOWNTURN, 1000000000.0)
+        dct0_runner = ZI_DCT0(self.logger, config)
+        price_series = [101, 100.9, 101.1, 100.5, 101.5, 100, 101]
+        observe = dct0_runner.observe(price_series[0])
+        self.assertEqual(DCEventType.DOWNTURN, dct0_runner.mode)
+        self.assertEqual(101, dct0_runner.p_ext)
+        self.assertEqual(DCEventType.OVERSHOOT, observe)
+        observe = dct0_runner.observe(price_series[1])
+        self.assertEqual(DCEventType.DOWNTURN, dct0_runner.mode)
+        self.assertEqual(100.9, dct0_runner.p_ext)
+        self.assertEqual(DCEventType.OVERSHOOT, observe)
+        observe = dct0_runner.observe(price_series[2])
+        self.assertEqual(DCEventType.DOWNTURN, dct0_runner.mode)
+        self.assertEqual(100.9, dct0_runner.p_ext)
+        self.assertEqual(DCEventType.OVERSHOOT, observe)
+        observe = dct0_runner.observe(price_series[3])
+        self.assertEqual(DCEventType.DOWNTURN, dct0_runner.mode)
+        self.assertEqual(100.5, dct0_runner.p_ext)
+        self.assertEqual(DCEventType.OVERSHOOT, observe)
+        observe = dct0_runner.observe(price_series[4])
+        self.assertEqual(DCEventType.DOWNTURN, dct0_runner.mode)
+        self.assertEqual(100.5, dct0_runner.p_ext)
+        self.assertEqual(DCEventType.OVERSHOOT, observe)
+        observe = dct0_runner.observe(price_series[5])
+        self.assertEqual(DCEventType.DOWNTURN, dct0_runner.mode)
+        self.assertEqual(100, dct0_runner.p_ext)
+        self.assertEqual(DCEventType.OVERSHOOT, observe)
+        observe = dct0_runner.observe(price_series[6])
+        self.assertEqual(DCEventType.UPTURN, dct0_runner.mode)
+        self.assertEqual(101, dct0_runner.p_ext)
+        self.assertEqual(DCEventType.UPTURN, observe)
+        pass
